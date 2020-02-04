7d329c7d1c02ca877a8d0f4c6538beafe86242b9
kentpei <48425664+kentpei@users.noreply.github.com>
2020-02-04 14:32:14 -0500

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+def getinfo1(x,y):
+    url = "http://api.openweathermap.org/data/2.5/weather?" + "lat=" + str(y) + "&lon=" + str(x) + "&APPID=98890b28aebc9ab089b941017e6a499a"
+    content = requests.get(url).content
+    new = content.decode("utf-8")
+    content_json = json.loads(new)
+    #print(new)
+    #name = content_json["name"]
+    temp , mintemp , maxtemp = content_json["main"]["temp"] , content_json["main"]["temp_min"] , content_json["main"]["temp_max"]
+    weather = content_json["weather"][0]["main"]
+    return ("weather:" + str(weather) + "  "  +  "temperatue:" + str(temp) + "  "  + "minimum temperature:" +
+            str(mintemp) + "  "  + "maximum temperature:" + str(maxtemp))
+def getinfo2(city):
+    url = "http://api.openweathermap.org/data/2.5/weather?" + "q=" + city + "&APPID=98890b28aebc9ab089b941017e6a499a"
+    content = requests.get(url).content
+    new = content.decode("utf-8")
+    content_json = json.loads(new)
+    #print(new)
+    #name = content_json["name"]
+    temp , mintemp , maxtemp = content_json["main"]["temp"] , content_json["main"]["temp_min"] , content_json["main"]["temp_max"]
+    weather = content_json["weather"][0]["main"]
+    return ("name:" + city + "  " + "weather:" + str(weather) + "  "  +  "temperatue:" + str(temp) + "  "  + "minimum temperature:" + str(mintemp) + "  "  +
+            "maximum temperature:" + str(maxtemp))
