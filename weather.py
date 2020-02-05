f77a174ed92a3e45acb6e11fe36a8929d8155f8a
kentpei <48425664+kentpei@users.noreply.github.com>
2020-02-05 17:48:20 -0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 def getinfo1(x,y):
-    url = "http://api.openweathermap.org/data/2.5/weather?" + "lat=" + str(y) + "&lon=" + str(x) + "&APPID=98890b28aebc9ab089b941017e6a499a"
+    url = "http://api.openweathermap.org/data/2.5/weather?" + "lat=" + str(y) + "&lon=" + str(x) + "&APPID=(your key's here)"
     content = requests.get(url).content
     new = content.decode("utf-8")
     content_json = json.loads(new)
@@ -13,7 +13,7 @@ def getinfo1(x,y):
     return ("weather:" + str(weather) + "  "  +  "temperatue:" + str(temp) + "  "  + "minimum temperature:" +
             str(mintemp) + "  "  + "maximum temperature:" + str(maxtemp))
 def getinfo2(city):
-    url = "http://api.openweathermap.org/data/2.5/weather?" + "q=" + city + "&APPID=98890b28aebc9ab089b941017e6a499a"
+    url = "http://api.openweathermap.org/data/2.5/weather?" + "q=" + city + "&APPID=9(your key's here)"
     content = requests.get(url).content
     new = content.decode("utf-8")
     content_json = json.loads(new)
