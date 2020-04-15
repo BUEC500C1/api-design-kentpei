b91f53c3e067bdd84c1c3efc25a68ad502932fe0
kentpei <48425664+kentpei@users.noreply.github.com>
2020-04-15 18:01:06 -0400

    Update weatherAPI.py

--- weatherAPI.py
+++ weatherAPI.py
@@ -15,32 +15,27 @@ def getdata():
     return dic
 def search_city(city):
     try:
-        print(getinfo2(city))
-        return 1
+        #print()
+        return getinfo2(city)
     except:
         return "Input Error"
 def search_airport(dic,airport):
     try:
         cor = dic[airport].split(",")
         info = getinfo1(float(cor[0]), float(cor[1]))
-        print(airport + "  " + info)
-        return 1
+        #print()
+        return airport + "  " + info
     except:
         return "Input Error"
-    '''
-    for i in dic:
-        cor = dic[i].split(",")
-        info = getinfo(float(cor[0]), float(cor[1]))
-        lst.append(str(i) + "  " + info)
-    '''
+
 '''
 dic = getdata()
 print(search_airport(dic,"General Edward Lawrence Logan International Airport"))
 print(search_city("Newyork"))
 
-search_airport(dic,"Total Rf Heliport")
-search_airport(dic,"Newark Liberty International Airport")
-search_airport(dic,"John F Kennedy International Airport")
-search_city("Boston")
-search_city("New York")
+print(search_airport(dic,"Total Rf Heliport"))
+print(search_airport(dic,"Newark Liberty International Airport"))
+print(search_airport(dic,"John F Kennedy International Airport"))
+print(search_city("Boston"))
+print(search_city("New York"))
 '''
