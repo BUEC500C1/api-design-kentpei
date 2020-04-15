4fcdc7923539130f299a5f80bcf1dc65529f2ee8
kentpei <48425664+kentpei@users.noreply.github.com>
2020-04-15 18:01:46 -0400

    Update test_weatherAPI.py

--- test_weatherAPI.py
+++ test_weatherAPI.py
@@ -1,11 +1,11 @@
 from weatherAPI import *
 def test():
     dic = getdata()
-    assert search_airport(dic,"General Edward Lawrence Logan International Airport") == 1
-    assert search_airport(dic,"Total Rf Heliport") == 1
+    assert search_airport(dic,"General Edward Lawrence Logan International Airport") != None
+    assert search_airport(dic,"Total Rf Heliport") != None
     assert search_airport(dic, "Total Rf") == "Input Error"
-    assert search_airport(dic,"Newark Liberty International Airport") == 1
-    assert search_airport(dic,"John F Kennedy International Airport") == 1
-    assert search_city("Boston") == 1
+    assert search_airport(dic,"Newark Liberty International Airport") != None
+    assert search_airport(dic,"John F Kennedy International Airport") != None
+    assert search_city("Boston") != None
     assert search_city("Newyork") == "Input Error"
-    assert search_city("New York") == 1
+    assert search_city("New York") != None
